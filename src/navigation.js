3e55ece07c967a8d3e6482aa1f3b15129b619ec5
karthikshetty <karhtikshetty1@gmail.com>
2023-06-09 23:45:52 +0530

    next

---
+++ src/navigation.js
@@ -0,0 +1,65 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import ProductsScreen from './screens/ProductsScreen';
+import ProductDetailsScreen from './screens/ProductDetailsScreen';
+import ShoppingCart from './screens/ShoppingCart';
+import { Pressable, Text } from 'react-native';
+
+import { FontAwesome5, MaterialCommunityIcons } from '@expo/vector-icons';
+import { useSelector } from 'react-redux';
+import { selectNumberOfItems } from './store/cartSlice';
+import TrackOrder from './screens/TrackOrder';
+
+const Stack = createNativeStackNavigator();
+
+const Navigation = () => {
+  const numberOfItems = useSelector(selectNumberOfItems);
+
+  return (
+    <NavigationContainer>
+      {/* root component */}
+      <Stack.Navigator
+        screenOptions={{ contentStyle: { backgroundColor: 'white' } }}
+        // it sets color to view that renders item
+      >
+        <Stack.Screen
+          name="WearCode"
+          component={ProductsScreen}
+          options={({ navigation }) => ({
+            headerRight: () => (
+              <Pressable
+                onPress={() => navigation.navigate('Cart')}
+                style={{ flexDirection: 'row' }}
+              >
+                <FontAwesome5 name="shopping-cart" size={18} color="gray" />
+                <Text style={{ marginLeft: 5, fontWeight: '500' }}>
+                  {numberOfItems}
+                </Text>
+              </Pressable>
+            ),
+            headerLeft: () => (
+              <MaterialCommunityIcons
+                onPress={() => navigation.navigate('Track Order')}
+                name="truck-delivery"
+                size={22}
+                color="gray"
+              />
+            ),
+
+          })}
+        />
+        <Stack.Screen
+          name="Product Details"
+          component={ProductDetailsScreen}
+          options={{ presentation: 'modal' }}
+        // modal repsresnts way it should appear when we touch on it in products
+        />
+        <Stack.Screen name="Cart" component={ShoppingCart} />
+        <Stack.Screen name="Track Order" component={TrackOrder} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
---
+++ src/screens/ProductDetailsScreen.js
@@ -0,0 +1,114 @@
+import {
+  StyleSheet,
+  View,
+  Image,
+  FlatList,
+  useWindowDimensions,
+  Text,
+  ScrollView,
+  Pressable,
+  ActivityIndicator,
+} from 'react-native';
+import products from '../data/products';
+import { useSelector, useDispatch } from 'react-redux';
+import { cartSlice } from '../store/cartSlice';
+import { useGetProductQuery } from '../store/apiSlice';
+
+const ProductDetailsScreen = ({ route }) => {
+  // const id = route.params.id;
+
+  // const { data, isLoading, error } = useGetProductQuery(id);
+  // const product = data?.data;
+
+  // const dispatch = useDispatch();
+
+  const { width } = useWindowDimensions();
+  // //it takes size of screen and send it to window
+
+  const addToCart = () => {
+    // dispatch(cartSlice.actions.addCartItem({ product }));
+    console.warn("added to cart")
+  };
+
+  // if (isLoading) {
+  //   return <ActivityIndicator />;
+  // }
+
+  // if (error) {
+  //   return <Text>Error fetching the product. {error.error}</Text>;
+  // }
+  const product = products[0]
+  return (
+    <View>
+      <ScrollView>
+        {/* it is added bcoz description gopes down and u cant see it and also the image to go up when we scroll it  , inorder to nsee we did */}
+        {/* Image Carousel */}
+        <FlatList
+          data={product.images}
+          renderItem={({ item }) => (
+            <Image source={{ uri: item }} style={{ width, aspectRatio: 1 }} />
+          )}
+          horizontal //we want view in horizontal mode so
+          showsHorizontalScrollIndicator={false} //it is used to remove scroll indicator
+          pagingEnabled //centers them and close images in center
+        />
+
+        <View style={{ padding: 20 }}>
+          {/* Title */}
+          <Text style={styles.title}>{product.name}</Text>
+
+          {/* Price */}
+          <Text style={styles.price}>${product.price}</Text>
+
+          {/* Description */}
+          <Text style={styles.description}>{product.description}</Text>
+        </View>
+      </ScrollView>
+
+      {/* Add to cart button */}
+      <Pressable onPress={addToCart} style={styles.button}>
+        {/* it handles press events */}
+        <Text style={styles.buttonText}>Add to cart</Text>
+      </Pressable>
+
+      {/* Navigation icon */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 34,
+    fontWeight: '500',
+    marginVertical: 10,
+  },
+  price: {
+    fontWeight: '500',
+    fontSize: 16,
+    letterSpacing: 1.5,
+  },
+  description: {
+    marginVertical: 10,
+    fontSize: 18,
+    lineHeight: 30,
+    fontWeight: '300',
+  },
+
+  button: {
+    position: 'absolute',
+    backgroundColor: 'black',
+    bottom: 30,
+    width: '90%',
+    alignSelf: 'center',
+    padding: 20,
+    borderRadius: 100,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '500',
+    fontSize: 16,
+  },
+});
+
+export default ProductDetailsScreen;
